3ba46a807af5676b27063b559e499619aebb076d
bantawa04 <rahunn3@gmail.com>
2022-06-23 16:17:34 +0545

    7.4 Grid and linear gradient

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
+import './category_item.dart';
+import './dummy_data.dart';
 
 class MyWidget extends StatelessWidget {
+  const MyWidget({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return GridView(
-      gridDelegate: const  SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20
-      ),
-      children: <Widget>[],
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20),
+      children: DUMMY_CATEGORIES
+          .map((item) => CategoryItem(item.title, item.color))
+          .toList(),
     );
   }
 }
