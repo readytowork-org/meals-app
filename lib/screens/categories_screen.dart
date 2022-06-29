256112ea7adc72824927c68e4c30b84e17730cac
bantawa04 <rahunn3@gmail.com>
2022-06-29 15:54:39 +0545

    7.20 Appbar tab

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -6,11 +6,7 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("DeliMeals"),
-      ),
-      body: GridView(
+    return GridView(
         padding: const EdgeInsets.all(10),
         gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
             maxCrossAxisExtent: 200,
@@ -20,7 +16,6 @@ class CategoriesScreen extends StatelessWidget {
         children: DUMMY_CATEGORIES
             .map((item) => CategoryItem(item.id, item.title, item.color))
             .toList(),
-      ),
-    );
+      );
   }
 }
